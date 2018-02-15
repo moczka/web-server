74908cb835d97172a5725faa2b3ad3d3d86d0010
Javier Guerrero <jguerrero@wayfair.com>
2018-02-15 17:16:23 -0500

    Add core files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =  require('express');
+const path = require('path');
+const app = express();
+const STATIC_CONTENT = '/static';
+
+app.get('/:view', (req, res) => {
+  const view = req.params['view'];
+  res.sendFile(path.join(`${__dirname}${STATIC_CONTENT}/${view}`));
+});
+app.get('/find', (req, res) => {
+
+  const responseText = Object.keys(req.query).reduce((message, key) => {
+    return `${message} \n ${key} : ${req.query[key]},`;
+  }, 'Your request includes: \n');
+ console.log(req.query);
+ res.send(responseText);
+});
+app.listen(3000, () => console.log('Running...!'));
